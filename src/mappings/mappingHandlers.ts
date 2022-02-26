95b7e8e7056c20ed36200243f705fc97f6be71f4
Bobo <bobo.kovacevic@gmail.com>
2022-02-26 23:54:43 +0100

    Offset from average block production calculation

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,5 +1,5 @@
 import {SubstrateBlock} from "@subql/types";
-import {Collator} from "../types";
+import {Collator, Day, BlockProduction} from "../types";
 import type { AccountId, Digest } from '@polkadot/types/interfaces';
 
 // https://github.com/polkadot-js/api/blob/beffc7b754dce576242a1b17da81c5ff61096631/packages/api-derive/src/type/util.ts#L6
@@ -39,19 +39,72 @@ function extractAuthor (digest: Digest, sessionValidators: AccountId[] = []): Ac
 
 function formatDate(date: Date): string {
   return date.toISOString().slice(0,10).replace(/-/g, '');
-
-  
 }
 
-async function handleStartEnd(block: SubstrateBlock): Promise<void> {
+function getBlockProductionKey(date: Date, author: AccountId): string {
+  return `${formatDate(date)}_${author.toString()}`;
+}
+
+async function handleDayStartEnd(block: SubstrateBlock): Promise<void> {
   const date = formatDate(block.timestamp);
+
+  let day = await Day.get(date);
+  if (!day) {
+    day = new Day(date);
+    day.firstBlock = block.block.header.number.toBigInt();
+    await day.save();
+
+    const prevDate = new Date(block.timestamp);
+    prevDate.setDate(prevDate.getDate() - 1);
+    const prevDay = await Day.get(formatDate(prevDate));
+    if (prevDay) {
+      prevDay.lastBlock = day.firstBlock - BigInt(1);
+      await prevDay.save();
+
+      await calculateMissedBlocks(prevDate);
+    }
+  }
 }
+
+async function calculateMissedBlocks(date: Date): Promise<void> {
+  if (validators) {
+    const day = await Day.get(formatDate(date));
+
+    if (day) {
+      const blocksProducedInDay = day.lastBlock - day.firstBlock + BigInt(1);
+      const avgPerValidator = Number(blocksProducedInDay.toString()) / validators.length;
+
+      validators.forEach(async validator => {
+        const id = getBlockProductionKey(date, validator);
+        const blockProduction = await BlockProduction.get(id);
+
+        if (blockProduction) {
+          blockProduction.avgBlockProductionOffset = blockProduction.blocksProduced - avgPerValidator;
+          await blockProduction.save();    
+        } else {
+          logger.warn(`calculateMissedBlocks:: no block production for ${validator} on ${date}`);
+        }
+      });
+    } else {
+      logger.warn(`calculateMissedBlocks:: no day defined for ${date}`);
+    }
+  } else {
+    logger.warn('calculateMissedBlocks:: no validators defined');
+  }
+}
+
+let validators: AccountId[];
 
 export async function handleBlock(block: SubstrateBlock): Promise<void> {
-    const validators = await api.query.session.validators();
+    await handleDayStartEnd(block);
+
+    if (!validators) {
+      validators = await api.query.session.validators();
+    }
     const author = extractAuthor(block.block.header.digest, validators);
 
     if (author) {
+      // aggregate total blocks produced by a collator
       let collator = await Collator.get(author.toString());
 
       if (!collator) {
@@ -61,8 +114,23 @@ export async function handleBlock(block: SubstrateBlock): Promise<void> {
 
       collator.blocksProduced++;
       await collator.save();
+
+      // aggregate total blocks produced per collator per day
+      const id = getBlockProductionKey(block.timestamp, author);
+      let blockProduction = await BlockProduction.get(id);
+
+      if (!blockProduction) {
+        blockProduction = new BlockProduction(id);
+        blockProduction.collatorId = author.toString();
+        blockProduction.dayId = formatDate(block.timestamp);
+        blockProduction.blocksProduced = 0;
+        blockProduction.avgBlockProductionOffset = 0;
+      }
+
+      blockProduction.blocksProduced++;
+      await blockProduction.save();
     
-      logger.info(`Block ${block.block.header.hash.toString()} ${block.timestamp} ${author}`);
+      // logger.info(`Block ${block.block.header.hash.toString()} ${block.timestamp} ${author}`);
     } else {
       logger.error(`Unable to extract collator address for block ${block.block.header.hash.toString()}`);
     }
